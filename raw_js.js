88a88095060de700de40d5617e1c9573f7e23a27
sai-sridhar-repo-07 <tarrasridhar1154@gmail.com>
2023-08-22 19:17:17 +0530

    Day-2

--- raw_js.js
+++ raw_js.js
@@ -6,6 +6,8 @@ toggle menu buttons
 
 const menu = document.querySelector('[data-id = "menu"]');
 const menuItems = document.querySelector('[data-id = "menu-items"]');
+const resetBtn = document.querySelector('[data-id = "reset-btn"]');
+const newRoundBtn = document.querySelector('[data-id = "new-round-btn"]');
 
 menu.addEventListener("click", () => {
   menuItems.classList.toggle("hidden");
